7a5d08f2c29b50244184ffd0fed922e821609763
ganesh kavhar <kavharg@gmail.com>
2025-10-16 09:09:24 +0530

    Create basic syntax n anithmatic.py

---
+++ basic syntax n anithmatic.py
@@ -0,0 +1,11 @@
+# Add two numbers
+add = lambda a, b: a + b
+print(add(5, 3))  # Output: 8
+
+# Square of a number
+square = lambda x: x * x
+print(square(4))  # Output: 16
+
+# Cube using lambda
+cube = lambda x: x ** 3
+print(cube(2))  # Output: 8
